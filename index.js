bcee3395143c1cca9d35ee26bd50b9eaaa310a0d
Sandy Merida <sandymeridah@gmail.com>
2022-02-26 06:23:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Software avanzado - PRACTICA 11 </h1>')
+  res.send('<h1>Software avanzado - A - Lab #6 </h1>')
 })
 
 app.listen(port, () => {
